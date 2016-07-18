3622e0af9a9061ecc6eb3f2ce6e31d9967623cbf
Jorge Nelson <jorgenelson@MacBook-Pro-van-Jorge-Nelson.local>
2016-07-18 09:23:02 +0200

    seed and new newsitem link

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+
+NewsItem.delete_all
+
+NewsItem.create(title: "Seed item", sub_header: "this is the sub header text", content: "this is the content text", image: "www.nu.nl")
